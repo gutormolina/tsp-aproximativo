499bebd421a053e549afb10dd7d252f5ec300a6c
Eduarda Louzada <116181107+dudalouzada@users.noreply.github.com>
2025-05-26 22:10:48 -0300

    Update tsp_solver.py

--- tsp_solver.py
+++ tsp_solver.py
@@ -31,3 +31,34 @@ def nearest_neighbors(matrix):
     tour.append(tour[0])
     cost = sum(matrix[tour[i]][tour[i+1]] for i in range(len(tour)-1))
     return tour, cost
+
+def cheapest_insertion(matrix):
+    n = len(matrix)
+    if n < 2:
+        return [0]*n, 0
+    
+    start = 0
+    nearest = min((c for c in range(1, n)), key=lambda c: matrix[start][c])
+    subtour = [start, nearest, start]
+    unvisited = set(range(n)) - set(subtour)
+    
+    while unvisited:
+        best_cost_increase = float('inf')
+        best_city = None
+        best_position = -1
+        
+        for city in unvisited:
+            for i in range(len(subtour)-1):
+                cost_increase = (matrix[subtour[i]][city] + 
+                                 matrix[city][subtour[i+1]] - 
+                                 matrix[subtour[i]][subtour[i+1]])
+                if cost_increase < best_cost_increase:
+                    best_cost_increase = cost_increase
+                    best_city = city
+                    best_position = i+1
+        
+        subtour.insert(best_position, best_city)
+        unvisited.remove(best_city)
+        
+    cost = sum(matrix[subtour[i]][subtour[i+1]] for i in range(len(subtour)-1))
+    return subtour, cost
